edf0252faa0e5d4b783a21f9bda5a51ddf909cee
swapnaganne99 <80024023+swapnaganne99@users.noreply.github.com>
2025-04-30 02:21:43 +0000

    chore: fix typos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import productData from './data/full-products';
-
+import Header from './components/Header'
+import CardList from './components/CardList';
+import SingleView from './components/SingleView';
+import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <h1>Hello There.</h1>
-    </div>
-  );
+      <Header />
+
+<Routes>
+  <Route path='/' element= { <CardList data={productData} />} />
+  <Route path="/product/:id" element={<SingleView data={productData} />}></Route>
+</Routes>
+</div>
+);
 }
 
 export default App;
